95a1e9e2197581b6ff2eb0e6376ad245bb2f2c39
YTRodi <yagorodi@gmail.com>
2022-04-23 00:59:50 -0300

    feat(screen): add styles in UnauthenticatedApp

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as FullPageErrorFallback } from './FullPageErrorFallback';
 export { default as FullPageSpinner } from './FullPageSpinner';
 export { default as IconButton } from './IconButton';
+export { default as GameBlock } from './GameBlock';
