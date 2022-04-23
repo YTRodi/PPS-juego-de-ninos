f4e8d582c6b53c797756eeb7b7c216a690cd0f33
YTRodi <yagorodi@gmail.com>
2022-04-23 01:59:39 -0300

    feat(component): add HardcodedPersons

---
+++ src/components/HardcodedPersons/index.tsx
@@ -0,0 +1,77 @@
+import { useMemo } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Paragraph } from 'react-native-paper';
+import GameBlock from '../GameBlock';
+import Separator from '../Separator';
+
+interface Person {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface Props {
+  onSelectPerson: (person: Person) => void;
+}
+
+const genericPassword = 'Password1';
+
+const HardcodedPersons = ({ onSelectPerson }: Props) => {
+  const persons = useMemo<Person[]>(
+    () => [
+      {
+        name: 'Scott',
+        email: 'scottR@yopmail.com',
+        password: genericPassword,
+      },
+      {
+        name: 'Maggie',
+        email: 'maggieR@yopmail.com',
+        password: genericPassword,
+      },
+      {
+        name: 'Luna',
+        email: 'lunaR@yopmail.com',
+        password: genericPassword,
+      },
+    ],
+    []
+  );
+
+  return (
+    <View>
+      <View style={styles.textContainer}>
+        <Separator />
+        <Paragraph style={styles.text}>inicia sesión como</Paragraph>
+        <Separator />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        {persons.map((person, index) => (
+          <Button key={index} onPress={() => onSelectPerson(person)}>
+            <GameBlock text={person.name} />
+          </Button>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  text: {
+    marginHorizontal: 16,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+});
+
+export default HardcodedPersons;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as FullPageErrorFallback } from './FullPageErrorFallback';
 export { default as FullPageSpinner } from './FullPageSpinner';
 export { default as IconButton } from './IconButton';
 export { default as GameBlock } from './GameBlock';
+export { default as HardcodedPersons } from './HardcodedPersons';
+export { default as Separator } from './Separator';
